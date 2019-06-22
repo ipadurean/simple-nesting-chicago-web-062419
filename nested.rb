005d0549a5b9cbfc7de0223b4bf84b36bbc27756
Iuliu Padurean <ciprian.padurean@gmail.com>
2019-06-22 19:25:24 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -18,7 +18,7 @@ def hopper
 
 
 end
-hopper(grace_hopper)
+hopper[0][0]
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
